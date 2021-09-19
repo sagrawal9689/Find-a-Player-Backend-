967bc9175bfc6b96c90c54456a7e26fd69be8087
unknown <sagrawal9689@gmail.com>
2021-09-19 12:12:51 +0530

    Initial server setup and databse connection

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import globalErrorHandler from './controllers/errorController.js'
+
+dotenv.config({path: './config.env'})
+
+connectDB()
+
+const app = express()
+
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+  res.send("Hello")
+})
+
+app.use('/api/users',userRoutes)
+
+app.use(globalErrorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+)
