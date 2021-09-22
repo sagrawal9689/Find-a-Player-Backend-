5e84737f89645650a54fa1c14c4c3f2c3985043b
unknown <sagrawal9689@gmail.com>
2021-09-22 15:51:03 +0530

    Post model created

---
+++ models/postModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+
+const postSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    gameName: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    from: {
+      type: Date,
+      required: true,
+    },
+    to: {
+        type: Date,
+        required: true,
+    },  
+    playersRequired: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model('Post', postSchema)
+
+export default Post
