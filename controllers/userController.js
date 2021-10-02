394a32ac2e821da074a0898afb0646b1af1e632c
unknown <sagrawal9689@gmail.com>
2021-10-02 13:04:25 +0530

    created route and controller for get Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,8 +66,17 @@ const registerUser = catchAsync (async(req, res,next) => {
   }
 })
 
+const getProfile = catchAsync (async(req, res,next) => {
+  const { name, email } = req.user
+
+  res.status(200).json({
+    name,
+    email
+  })
+})
 
 export{
   registerUser,
-  authUser
+  authUser,
+  getProfile
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,13 @@ import express from 'express'
 const router = express.Router()
 import {
   authUser,
-  registerUser
+  registerUser,
+  getProfile
 } from '../controllers/userController.js'
-
+import { protect } from './../middlewares/authMiddleware.js'
 
 router.route('/login').post(authUser)
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect,getProfile)
 
 export default router
