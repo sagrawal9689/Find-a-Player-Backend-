b253d05f5565dedb21c88cf9a03f72637de73147
unknown <sagrawal9689@gmail.com>
2021-10-03 13:13:23 +0530

    created route for getUserPost

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,13 +2,16 @@ import express from 'express'
 const router = express.Router()
 import {
   getPosts,
-  createPost
+  createPost,
+  getUserPosts
 } from '../controllers/postController.js'
 import { addRequest } from './../controllers/requestController.js';
 
 import { protect } from '../middlewares/authMiddleware.js'
 
 router.route('/').get(getPosts).post(protect,createPost)
+
+router.route('/:id').get(protect,getUserPosts)
 
 router.route('/request/:id').post(protect, addRequest)
 
